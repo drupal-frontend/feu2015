b39829246723dbd9c4c1ec25e3e6989ba5e1be97
Jannick Vandaele <jannickvandaele@hotmail.com>
2015-09-25 12:42:18 +0200

    custom timing function to detect 80% complete

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
+var picture_showing = false; 
+function mytiming(frame){
+  if(!picture_showing && frame > 0.8){
+    $(".bg-img").fadeTo( "slow" , 1);
+  }
+  return frame;
+}
+
 $(window).load(function(){
   $("#my-svg").css('opacity', '1');
   new Vivus('my-svg', {
     start: 'autostart',
     type: 'delayed',
     duration: 120,
-    animTimingFunction: Vivus.EASE,
-    onReady: function (myVivus) {
-      // Start timeout on ready, so every browser renders at same time.
-      setTimeout(function(){
-        $(".bg-img").fadeTo( "slow" , 1);
-      }, 1500);
-    }
+    animTimingFunction: mytiming
   }, function(){
     $(".svg-container").fadeTo( "slow" , 0);
   });
-
-
 });
